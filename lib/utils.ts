678548ef4e6ab8cae046486b1acb9ebe26070068
rakitha17 <rakithabimsara505@gmail.com>
2026-01-08 13:21:43 +0530

    dev-r17: utility file updated with pdfDownloader and excelDownloader

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,8 @@ import { twMerge } from 'tailwind-merge';
 import Cryptr from 'cryptr';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
+import ExcelJS from 'exceljs';
+import { saveAs } from 'file-saver';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -30,6 +32,7 @@ export function generateCode() {
 export const padCode = (num: number, length: number) =>
   num.toString().padStart(length, '0');
 
+// ==== PDF DOWNLOAD HANDLE UTIL ==== //
 type DownloadPdfOptions<T> = {
   title?: string;
   data: T[];
@@ -66,4 +69,52 @@ export const downloadPdfUtil = <T>({
   });
 
   doc.save(fileName);
+};
+
+// ==== EXCEL DOWNLOAD HANDLE UTIL ==== //
+type DownloadExcelOptions<T> = {
+  columns: string[];
+  data: T[];
+  keys: (keyof T)[];
+  fileName?: string;
+  title: string;
+};
+
+export const downloadExcelUtil = async <T>({
+  columns,
+  data,
+  keys,
+  fileName = 'report.xlsx',
+  title
+}: DownloadExcelOptions<T>) => {
+  const workbook = new ExcelJS.Workbook();
+  const sheet = workbook.addWorksheet(title);
+
+  /*  sheet.columns = [
+    { header: 'Name', key: 'name', width: 20 },
+    { header: 'Age', key: 'age', width: 10 }
+  ]; */
+
+  // == COLUMNS == //
+  sheet.columns = columns.map((column, index) => ({
+    header: column,
+    key: keys[index] as string,
+    width: 20
+  }));
+
+  // == ROWS == //
+  data.forEach((item) => {
+    const row: Record<string, any> = {};
+
+    keys.forEach((key) => {
+      row[key as string] = item[key];
+    });
+
+    sheet.addRow(row);
+  });
+
+  sheet.getRow(1).font = { bold: true };
+
+  const buffer = await workbook.xlsx.writeBuffer();
+  saveAs(new Blob([buffer]), fileName);
 };
