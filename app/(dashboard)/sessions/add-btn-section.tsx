2aec8e6ef75918dca4678f8fce18543afe7907b9
rakitha17 <rakithabimsara505@gmail.com>
2026-02-05 10:26:00 +0530

    dev-r17: sessions: layout button section changes

--- app/(dashboard)/sessions/add-btn-section.tsx
+++ app/(dashboard)/sessions/add-btn-section.tsx
@@ -13,13 +13,17 @@ import {
 import SessionsForm from './sessions-form';
 import { useSession } from 'next-auth/react';
 
+type ModalMode = 'ONE_DOCTOR' | 'ALL_DOCTOR';
+
 function GenerateSession({
   open,
   setModalOpen,
-  formData
+  formData,
+  mode
 }: {
   open: boolean;
   setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  mode: 'ONE_DOCTOR' | 'ALL_DOCTOR';
   formData: {
     options: { id: string; name: string }[];
   };
@@ -27,27 +31,29 @@ function GenerateSession({
   const { data } = useSession();
   const { doctor } = useSessionStore();
 
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
+  const handleCloseModal = () => setModalOpen(false);
 
   return (
     <AlertDialog open={open}>
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle>Create Session</AlertDialogTitle>
+          <AlertDialogTitle>
+            {mode === 'ALL_DOCTOR'
+              ? 'Create Sessions for All Doctors'
+              : 'Create Session'}
+          </AlertDialogTitle>
         </AlertDialogHeader>
-        <div>
-          <SessionsForm
-            onClose={handleCloseModal}
-            doctorId={doctor.id}
-            doctorOptions={formData.options}
-            user={{
-              id: data?.user?.id,
-              name: data?.user?.name || ''
-            }}
-          />
-        </div>
+
+        <SessionsForm
+          type={mode}
+          doctorId={mode === 'ONE_DOCTOR' ? doctor.id : undefined}
+          doctorOptions={formData.options}
+          onClose={handleCloseModal}
+          user={{
+            id: data?.user?.id,
+            name: data?.user?.name || ""
+          }}
+        />
       </AlertDialogContent>
     </AlertDialog>
   );
@@ -60,28 +66,37 @@ export default function AddBtnSection({
     options: { id: string; name: string }[];
   };
 }) {
-  const { doctor } = useSessionStore();
   const [modalOpen, setModalOpen] = React.useState(false);
+  const [modalMode, setModalMode] = React.useState<ModalMode>('ALL_DOCTOR');
 
   return (
     <>
       <Button
         size="sm"
-        onClick={() => setModalOpen(true)}
-        className="gap-1 px-8 text-white transition-colors ease-in-out duration-100 hover:text-black"
+        onClick={() => {
+          setModalMode('ALL_DOCTOR');
+          setModalOpen(true);
+        }}
       >
-        <PlusCircle />
-        <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-          Generate Session
-        </span>
+        Generate Sessions for all the doctors
       </Button>
+
+      <Button
+        size="sm"
+        onClick={() => {
+          setModalMode('ONE_DOCTOR');
+          setModalOpen(true);
+        }}
+      >
+        Generate Session
+      </Button>
+
       {/* ==== ALERT DIALOG ==== */}
       <GenerateSession
         open={modalOpen}
         setModalOpen={setModalOpen}
-        formData={{
-          options: formData.options
-        }}
+        mode={modalMode}
+        formData={{ options: formData.options }}
       />
     </>
   );
