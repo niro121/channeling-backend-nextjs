6840cf79977238a829216bcd319942d71626d1df
rakitha17 <rakithabimsara505@gmail.com>
2026-02-17 12:59:37 +0530

    dev-r17: 2FA: step

---
+++ types/2FA.ts
@@ -0,0 +1,10 @@
+type Option = {
+  id: string
+  option: string
+}
+
+export const TWO_FACTOR_AUTH: Option[] = [
+  {id: '1', option: 'AUTH-APP'},
+  {id: '2', option: 'SMS'},
+  {id: '3', option: 'EMAIL'},
+]
